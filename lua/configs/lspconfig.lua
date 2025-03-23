6397054985e0f766aab61a04c96c0ddc54a88e76
ynoacamino <ynoacamino@gmail.com>
2025-03-23 10:29:23 -0500

    add list of language server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,6 +3,18 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
+lspconfig.servers = {
+  "css-lsp",
+  "docker-compose-language-service",
+  "dockerfile-language-server",
+  "eslint-lsp",
+  "gopls",
+  "html-lsp",
+  "lua-language-server",
+  "tailwindcss-language-server",
+  "typescript-language-server",
+}
+
 -- EXAMPLE
 local servers = { "html", "cssls", "gopls", "ts_ls", "tailwindcss", "dockerls", "docker_compose_language_service" }
 local nvlsp = require "nvchad.configs.lspconfig"
